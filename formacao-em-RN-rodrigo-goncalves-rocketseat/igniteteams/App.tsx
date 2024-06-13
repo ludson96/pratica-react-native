bd2c09d0264fc940361e72b0d4b831f7f4b3a6af
Ludson Pereira <ludson_ps27@hotmail.com>
2024-06-13 12:01:03 -0300

    feat: :sparkles: add loading personalizado

--- formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/App.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/App.tsx
@@ -1,12 +1,13 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ThemeProvider } from 'styled-components';
 
 import Groups from './src/screens/Groups';
 import theme from './src/theme';
 
+import Loading from '~/components/Loading';
+
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Groups />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return <ThemeProvider theme={theme}>{!fontsLoaded ? <Groups /> : <Loading />}</ThemeProvider>;
 }
--- formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/screens/Groups/style.ts
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/screens/Groups/style.ts
@@ -1,4 +1,3 @@
-import { Roboto_100Thin } from '@expo-google-fonts/roboto';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
