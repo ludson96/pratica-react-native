c8199eb36dea522e9b07c3ede1ceab2acb4e2a8f
Ludson Pereira <ludson_ps27@hotmail.com>
2024-06-14 10:41:55 -0300

    feat: :sparkles: add icon return and styles

--- formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/components/Header/index.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/components/Header/index.tsx
@@ -1,9 +1,19 @@
-import { Container, Logo } from './style';
+import { Container, Logo, BackIcon, BackButton } from './style';
 import logoImg from '../../assets/logo.png';
 
-export default function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export default function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logoImg} />
     </Container>
   );
