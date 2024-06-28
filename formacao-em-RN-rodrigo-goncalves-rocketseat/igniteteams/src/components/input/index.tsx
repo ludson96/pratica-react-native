7a978fc717447c3cc5c428f8844c094b8e77dae4
Ludson Pereira <ludson_ps27@hotmail.com>
2024-06-28 10:57:27 -0300

    refactor: :recycle: refatora estilos, colocando apenas uma arrow para todas as chamadas de função

--- formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/components/input/index.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/components/input/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components/native';
 
 import { Container } from './style';
 
 export default function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
