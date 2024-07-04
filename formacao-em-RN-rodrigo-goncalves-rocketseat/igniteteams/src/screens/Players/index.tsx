776c86c3403db34d2fc39a4952f9e1adfa048f65
Ludson Pereira <ludson_ps27@hotmail.com>
2024-07-04 11:55:22 -0300

    feat: :sparkles: add teams in list

--- formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/screens/Players/index.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/screens/Players/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FlatList } from 'react-native';
 
 import { Container } from './style';
 
 import ButtonIcon from '~/components/ButtonIcon';
 import { Form } from '~/components/ButtonIcon/styles';
+import Filter from '~/components/Filter';
 import Header from '~/components/Header';
 import Highlight from '~/components/Highlight';
 import Input from '~/components/input';
@@ -19,6 +21,13 @@ export default function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <FlatList
+        data={['Time A', 'Time B']}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <Filter title={item} />}
+        horizontal
+      />
     </Container>
   );
 }
