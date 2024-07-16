6fdad6ca4a144717b04ecd32b7558dc8d3b191f8
Ludson Pereira <ludson_ps27@hotmail.com>
2024-07-16 11:31:26 -0300

    feat: :sparkles: create function add player by group

--- formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/screens/Players/index.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/igniteteams/src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
 import { useRoute } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 
 import { Container, HeaderList, NumberOfPlayers } from './style';
 
@@ -13,6 +13,8 @@ import Highlight from '~/components/Highlight';
 import ListEmpty from '~/components/ListEmpty';
 import PlayerCard from '~/components/PlayerCard';
 import Input from '~/components/input';
+import { playerAddByGroup } from '~/storage/player/playerAddByGroup';
+import { AppError } from '~/utils/AppError';
 
 type RouteParams = {
   group: string;
@@ -21,9 +23,32 @@ type RouteParams = {
 export default function Players() {
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState(['Ludson']);
+  const [newPlayerName, setNewPlayerName] = useState('');
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  async function handleAddPLayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message);
+      } else {
+        console.log(error);
+        Alert.alert('Nova pessoa', 'Não foi possivel adicionar');
+      }
+    }
+  }
 
   return (
     <Container>
@@ -32,8 +57,8 @@ export default function Players() {
       <Highlight title={group} subtitle="Adicione a galera e separe os times" />
 
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input placeholder="Nome da pessoa" autoCorrect={false} onChangeText={setNewPlayerName} />
+        <ButtonIcon icon="add" onPress={handleAddPLayer} />
       </Form>
 
       <HeaderList>
