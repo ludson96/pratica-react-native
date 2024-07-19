848bc39b8b9c3a6021d422b518236723c18e48e8
Ludson Pereira <ludson_ps27@hotmail.com>
2024-07-19 10:59:16 -0300

    feat: :sparkles: add style for status bar

--- formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/App.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/App.tsx
@@ -1,7 +1,18 @@
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  return <View>{fontsLoaded ? <Text>Hello world</Text> : <View />}</View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Text>Hello world</Text> : <View />}
+    </View>
+  );
 }
