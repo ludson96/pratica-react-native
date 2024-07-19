ebef23936fb8ca3ea9413aa8b599b6fe73c8e821
Ludson Pereira <ludson_ps27@hotmail.com>
2024-07-19 10:51:34 -0300

    feat: :sparkles: add default font

--- formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/App.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/App.tsx
@@ -1,11 +1,7 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
-  );
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return <View>{fontsLoaded ? <Text>Hello world</Text> : <View />}</View>;
 }
