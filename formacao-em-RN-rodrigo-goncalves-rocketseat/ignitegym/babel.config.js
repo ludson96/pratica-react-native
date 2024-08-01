10d3ae1d7554de43283b59116793e5b1731effa5
Ludson Pereira <ludson_ps27@hotmail.com>
2024-08-01 12:03:31 -0300

    feat: :tada: reset project

--- formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/babel.config.js
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/babel.config.js
@@ -1,27 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
-  const plugins = [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@': './',
-        },
-      },
-    ],
-  ];
-
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: 'nativewind',
-        },
-      ],
-      'nativewind/babel',
-    ],
-    plugins,
+    presets: ['babel-preset-expo'],
   };
 };
