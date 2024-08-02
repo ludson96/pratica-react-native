0b34f1f4fc0a00c3949ab00e06f9131a0ece6b27
Ludson Pereira <ludson_ps27@hotmail.com>
2024-08-02 09:47:14 -0300

    feat: :lipstick: customizate status bar react native

--- formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/App.tsx
+++ formacao-em-RN-rodrigo-goncalves-rocketseat/ignitegym/App.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#111111'}}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoader ? <Text>Tá carregado</Text> : <View />}
     </View>
   );
